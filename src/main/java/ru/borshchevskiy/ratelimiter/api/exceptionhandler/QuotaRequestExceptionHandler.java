14ac403077c1a7ffdb1878d3eeb78d8ebd8063f9
Ilya <freer1der.msf@gmail.com>
2024-04-09 18:02:39 +0300

    Added QuotaRequestExceptionHandler for QuotaController

--- src/main/java/ru/borshchevskiy/ratelimiter/api/exceptionhandler/QuotaRequestExceptionHandler.java
+++ src/main/java/ru/borshchevskiy/ratelimiter/api/exceptionhandler/QuotaRequestExceptionHandler.java
@@ -1,2 +1,23 @@
-package ru.borshchevskiy.ratelimiter.api.exceptionhandler;public class QuotaRequestExceptionHandler {
+package ru.borshchevskiy.ratelimiter.api.exceptionhandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import ru.borshchevskiy.ratelimiter.api.controller.QuotaController;
+import ru.borshchevskiy.ratelimiter.api.exception.MissingOperationIdException;
+import ru.borshchevskiy.ratelimiter.api.exception.OperationIdNotFoundException;
+
+@ControllerAdvice(assignableTypes = QuotaController.class)
+public class QuotaRequestExceptionHandler {
+
+    @ExceptionHandler(value = OperationIdNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void handleOperationIdNotFoundException() {
+    }
+
+    @ExceptionHandler(value = MissingOperationIdException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void handleMissingOperationIdException() {
+    }
 }
