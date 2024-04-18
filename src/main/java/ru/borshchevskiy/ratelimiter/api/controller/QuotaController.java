91f945fa5906bb24b97eefc5b8976e41918febb1
Ilya <freer1der.msf@gmail.com>
2024-04-18 13:28:17 +0300

    Integrated QuotaService in QuotaController

--- src/main/java/ru/borshchevskiy/ratelimiter/api/controller/QuotaController.java
+++ src/main/java/ru/borshchevskiy/ratelimiter/api/controller/QuotaController.java
@@ -1,27 +1,34 @@
 package ru.borshchevskiy.ratelimiter.api.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import ru.borshchevskiy.ratelimiter.exception.BadRequestException;
-import ru.borshchevskiy.ratelimiter.exception.NotFoundException;
+import ru.borshchevskiy.ratelimiter.service.quota.QuotaService;
 
 @RestController
 public class QuotaController {
 
+    private final QuotaService tokenBucketQuotaService;
+
+    public QuotaController(QuotaService tokenBucketQuotaService) {
+        this.tokenBucketQuotaService = tokenBucketQuotaService;
+    }
+
     @GetMapping("/quota/{operation_id}")
     @ResponseStatus(HttpStatus.OK)
     public void getQuota(@PathVariable(value = "operation_id") String operationId) {
-        // TODO: test implementation, add logic
-        if (operationId.equals("test_not_found")) {
-            throw new NotFoundException();
-        }
+        tokenBucketQuotaService.consumeQuotaRequest(operationId);
     }
 
     /**
      * Method handles quota requests in situations when path segment /{operation_id} is not present.
      * In this case request is considered incorrectly formed and
      * a new {@link BadRequestException BadRequestException} is thrown.
-     * @exception BadRequestException if path segment /operation_id} is missing.
+     *
+     * @throws BadRequestException if path segment /operation_id} is missing.
      */
     @GetMapping(value = {"/quota", "/quota/"})
     public void getQuota() {
